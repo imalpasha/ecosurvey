c679c62df83ee51043a63b78397a81ee4b8e4776
Imal Pasha <imalpasha@gmail.com>
2018-02-01 18:16:10 +0800

    no message

---
+++ app/src/main/java/com/app/ecosurvey/ui/Model/Request/SurveyPhotoContentRequest.java
@@ -0,0 +1,37 @@
+package com.app.ecosurvey.ui.Model.Request;
+
+/**
+ * Created by imalpasha on 01/02/2018.
+ */
+
+public class SurveyPhotoContentRequest {
+
+    private String locationCode;
+    private String locationName;
+    private String icnumber;
+
+    public String getIcnumber() {
+        return icnumber;
+    }
+
+    public void setIcnumber(String icnumber) {
+        this.icnumber = icnumber;
+    }
+
+    public String getLocationCode() {
+        return locationCode;
+    }
+
+    public void setLocationCode(String locationCode) {
+        this.locationCode = locationCode;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+}
--- app/src/main/java/com/app/ecosurvey/ui/Model/Request/SurveyPhotoRequest.java
+++ app/src/main/java/com/app/ecosurvey/ui/Model/Request/SurveyPhotoRequest.java
@@ -2,6 +2,7 @@ package com.app.ecosurvey.ui.Model.Request;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import okhttp3.MultipartBody;
@@ -12,11 +13,26 @@ import okhttp3.MultipartBody;
 
 public class SurveyPhotoRequest {
 
-    private String icnumber;
-    private String locationCode;
-    private String locationName;
+    private String stringContent;
     private List<MultipartBody.Part> parts;
     private String token;
+    HashMap<String, Object> map;
+
+    public HashMap<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(HashMap<String, Object> map) {
+        this.map = map;
+    }
+
+    public String getStringContent() {
+        return stringContent;
+    }
+
+    public void setStringContent(String stringContent) {
+        this.stringContent = stringContent;
+    }
 
     public List<MultipartBody.Part> getParts() {
         return parts;
@@ -33,42 +49,5 @@ public class SurveyPhotoRequest {
     public void setToken(String token) {
         this.token = token;
     }
-
-
-
-    public String getIcnumber() {
-        return icnumber;
-    }
-
-    public void setIcnumber(String icnumber) {
-        this.icnumber = icnumber;
-    }
-
-    public String getLocationCode() {
-        return locationCode;
-    }
-
-    public void setLocationCode(String locationCode) {
-        this.locationCode = locationCode;
-    }
-
-    public String getLocationName() {
-        return locationName;
-    }
-
-    public void setLocationName(String locationName) {
-        this.locationName = locationName;
-    }
-
-    public String getLocationType() {
-        return locationType;
-    }
-
-    public void setLocationType(String locationType) {
-        this.locationType = locationType;
-    }
-
-    private String locationType;
-
 
 }
